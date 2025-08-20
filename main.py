28ba6d2265228aca0ebbf00cd9db2c628e1c9d24
Anson Teng <Ansonteng.mkg@gmail.com>
2025-08-20 16:40:04 +0800

    Changes made to main.py: Dummy Data

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
+
+#Fake data for testing api
+fake_data = [{"id": 1, "task":"Buy milk", "Completed": False},
+             {"id": 2, "task":"Walk Dog", "Completed": True}]
+
+
 @app.get("/")
-async def root():
-    return {"message": "TO_DO API"}
+def hello():
+    return {"message": "My Todo API is working!"}
+
+@app.get("/todos")
+def get_all_todos():
+    return fake_data
+
+@app.get("/todos/{todo_id}")
+def get_one_todo(todo_id: int):
+    for todo in fake_data:
+        if todo["id"] == todo_id:
+            return todo
+    return {"error": "Todo not found"}    
+        
+
+
